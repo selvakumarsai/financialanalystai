4e69c5b5966a518110d8702a6728e0ea4faf2f24
selvakumarsai <88017125+selvakumarsai@users.noreply.github.com>
2025-06-03 16:29:48 +1000

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from datetime import datetime, timedelta
 from langchain_core.tools import tool
 from langchain_core.messages import SystemMessage, HumanMessage
 from langgraph.prebuilt import create_react_agent
-
-from openbb_charting import Charting
+from numpy import NaN as npNaN
+#from openbb_charting import Charting
 #from openbb.app.model.charts.chart import ChartFormat
 
 # Suppress warnings from OpenBB
